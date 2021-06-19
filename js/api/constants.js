2345e1eadee54b63c899204f6615208750016a40
RobertSajdak <robert.sajdak@onet.eu>
2021-06-19 12:23:48 +0200

    Done

---
+++ js/api/constants.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://todo-api.coderslab.pl/api";
+export const API_KEY = "9f4a548b-8c44-4714-a57c-74a0f8634423";
--- js/app.js
+++ js/app.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
+import NewTask from "./components/NewTask";
+import Task from "./components/Task";
+import {getTasks} from "./api/tasks";
 
 function App() {
-  return <h1>Hello World</h1>
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    /**
+     * After component mount fetch all tasks from API
+     * @function getTasks - API function
+     */
+    getTasks(setTasks);
+  }, []);
+
+  /**
+   * Add new task local state
+   * @param {Object} task - Complete object with task details
+   * @param {string} task.title - Task title
+   * @param {string} task.description - Task description
+   * @param {string} task.status - Task status (open/closed)
+   */
+
+  const handleAddNewTask = task => {
+    setTasks(prevTasks => {
+      return [
+        task,
+        ...prevTasks
+      ];
+    });
+  };
+
+  /**
+   * Remove task from local state
+   * @param {string} id - ID of task
+   */
+
+  const handleRemoveTask = id => {
+    setTasks(prevState => prevState.filter(task => task.id !== id));
+  };
+
+  return (
+      <>
+        <NewTask onNewTask={handleAddNewTask}/>
+
+        {tasks.map(task => {
+          return <Task key={task.id} {...task} onRemoveTask={handleRemoveTask}/>;
+        })}
+      </>
+  );
 }
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
